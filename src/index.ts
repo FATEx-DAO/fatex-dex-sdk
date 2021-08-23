dddbd64b251d3a2dcd0890841985e70fda4444e5
Corey Caplan <coreycaplan3@gmail.com>
2021-08-23 02:38:08 -0400

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,16 @@ export {
   BigintIsh,
   Blockchain,
   ChainId,
+  PairType,
   TradeType,
   Rounding,
   FACTORY_ADDRESSES,
   INIT_CODE_HASH,
-  MINIMUM_LIQUIDITY
+  MINIMUM_LIQUIDITY,
+  SUSHI_FACTORY_ADDRESSES,
+  SUSHI_INIT_CODE_HASH,
+  VIPER_FACTORY_ADDRESSES,
+  VIPER_INIT_CODE_HASH,
 } from './constants'
 
 export * from './errors'
